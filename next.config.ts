ae31731b54559252e4bcf8c6d327a95aa2b25fd7
Omair Alam <omairalam508@gmail.com>
2026-06-05 14:50:31 -0400

    Change output to export

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  reactCompiler: true
+  reactCompiler: true,
+  output: "export"
 };
 
 export default nextConfig;
